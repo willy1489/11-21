9c4b2365d4f48a7c8816f97d9238b5b2a109c9f6
willy <willy0205willy@gmail.com>
2024-11-21 10:15:36 +0800

    copy new template

---
+++ ch6-2-1.php
@@ -0,0 +1,18 @@
+<?php 
+// 函數顯示標題文字
+function pageHeader() {
+   print "<h3>PHP與MySQL網頁設計</h3>";
+   echo "<hr/>";
+} 
+// 函數顯示註腳文字
+function pageFooter() {
+   print "<hr/>C111181133 謝文睿<br/>";
+} 
+?>
+</head>
+<body>
+<?php 
+pageHeader();  // 呼叫函數
+print "<p>在PHP程式使用函數建立標題和註腳文字</p>";
+pageFooter();  // 呼叫函數
+?>
